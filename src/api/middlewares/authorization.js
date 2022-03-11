80ebd6f7075b0a71057fccccad5db46c78df4db4
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-03-11 16:44:28 -0300

    middlewares alpha

--- src/api/middlewares/authorization.js
+++ src/api/middlewares/authorization.js
@@ -1,21 +1,21 @@
 const { checkToken } = require('../../utils/security');
 
 module.exports = {
-    tokenVerify : (req, res, next) => {
+    checkToken : (req, res, next) => {
         const authorization = req.headers.authorization;
 
         if (!authorization) 
             return res.status(401).send({error : 'token is null'});
         
-        const [bearer, token] = authorization.split(' ');
+        const [ bearer, token ] = authorization.split(' ');
         
         if (bearer !== 'Bearer') 
             return res.status(401).send({error : "expected gives bearer"});
-        
+
         try {
             const userData = checkToken(token);
             req.userData = userData;
-
+            req.permissions = userData.permissions
             next();
 
         } catch (error) {
@@ -23,12 +23,10 @@ module.exports = {
         }
     },
     checkPermissions : (req, res, next) => {
-        const permissions = req.body.permissions;
+        const permissions = req.permissions;
 
         if (!permissions || permissions.length === 0) 
             return res.status(401).send({ error : 'user has no permissions'})
-        
-
         next();
     }  
 }
