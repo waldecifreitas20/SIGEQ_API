43ed8f7aefe62dec4499c6f1b403ef8757acb2e2
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-03-24 11:57:17 -0300

    to do equipment test

--- __tests__/integration/services/equipment.test.js
+++ __tests__/integration/services/equipment.test.js
@@ -6,9 +6,23 @@ const factory = require('../../factory');
 describe('Create test', () => {
     const equipment = factory.generateEquipment();
 
-    it('should register a new equipment in database', async () => {
+    it('should register a new equipment in database with success', async () => {
         const response = await services.createEquipment(equipment);
         console.log(response);
         expect(response.status).toBe(200);
     });
+    
+    it('should return status 401 when trying create new equioment without token', async () => {
+
+    });
+    
+    it('should return status 401 when trying create new equipment without create permission', async () => {
+
+    });
 });
+
+describe('Delete equipment test', () => {});
+describe('Get by heritage equipment test', () => {});
+describe('Get all equipment test', () => {});
+describe('Update equipment test', () => {});
+
