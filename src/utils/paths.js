4283ae5ec8a96d64b89d6f658e45c0984e5c8d4e
Waldeci Freitas <freitas.wal18@gmail.com>
2022-04-19 15:55:34 -0300

    refactor security util

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -1,32 +1,38 @@
 const { resolve: getPath } = require('path');
 
 module.exports = {
+
+    database: getPath('src', 'database', 'db'),
+
     models: {
         index: getPath('src', 'api', 'models'),
         equipment: getPath('./src/api/models/equipment/Equipment.js'),
         user: getPath('src', 'api', 'models', 'user', 'User'),
         permission: getPath('src', 'api', 'models', 'user', 'Permission')
     },
+
     services: {
         index: getPath('src', 'api', 'services'),
         auth: getPath('src', 'api', 'services', 'authServices'),
         equipment: getPath('src', 'api', 'services', 'equipmentServices'),
     },
+
     middlewares: {
         index: getPath('src', 'api', 'middlewares'),
         formValidation: getPath('src', 'api', 'middlewares', 'formValidation'),
         authorization: getPath('src', 'api', 'middlewares', 'authorization'),
     },
+
     controllers: {
         index: getPath('src', 'api', 'controllers'),
         auth: getPath('src', 'api', 'controllers', 'auth'),
         equipment: getPath('src', 'api', 'controllers', 'equipment'),
     },
+
     routes: {
         index: getPath('src', 'routes'),
         auth: getPath('src', 'routes', 'auth'),
         equipment: getPath('src', 'routes', 'equipment'),
     },
-    repositories: getPath('src', 'api', 'repositories'),
-    database: getPath('src', 'database', 'db'),
+
 }
--- src/utils/security.js
+++ src/utils/security.js
@@ -2,22 +2,25 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 require('../config/dotenv');
 
-function generateToken(userData) {
-    return jwt.sign(userData, process.env.API_SECRET, {
-        expiresIn: 3600
-    });
-}
 
-function checkToken(token) {
-    return jwt.verify(token, process.env.API_SECRET);
-}
 
-function checkPassword(password, hash) {
-    return bcrypt.compareSync(password, hash);
-}
+
+
+
 
 module.exports = {
-    generateToken: generateToken,
-    checkToken: checkToken,
-    checkPassword: checkPassword
+    
+    generateToken: function (userData) {
+        return jwt.sign(userData, process.env.API_SECRET, {
+            expiresIn: 3600
+        });
+    },
+
+    checkToken: function (token) {
+        return jwt.verify(token, process.env.API_SECRET);
+    },
+
+    checkPassword: function (password, hash) {
+        return bcrypt.compareSync(password, hash);
+    }
 }
