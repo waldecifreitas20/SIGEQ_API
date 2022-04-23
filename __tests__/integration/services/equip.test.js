50488816a10fd29700a0c5ef4d7fe4ce34e034dc
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-04-23 20:44:11 -0300

    update equip services test

--- __tests__/integration/services/equip.test.js
+++ __tests__/integration/services/equip.test.js
@@ -9,35 +9,74 @@ describe('Create test', () => {
 
     it('should register a new equipment in database with success', async () => {
         const response = await services.createEquipment(equipment);
+
         expect(response.status).toBe(200);
     });
 
     it('should return status 400 when trying create already exists equipment', async () => {
         const response = await services.createEquipment(equipment);
+
         expect(response.status).toBe(400);
     });
 
     it('should return status 400 when trying create a null in database', async () => {
         const response = await services.createEquipment(undefined);
+
         expect(response.status).toBe(400);
     });
 });
 
+
+const _generateValidEquipmentId = async () => {
+    const { equipment_id: id } = await services.createEquipment(factory.generateEquipment());
+    return id;
+}
+
 describe('Delete test', () => {
 
-    it('should return status 200 when trying delete a equipment using their id', async () => {
+    it('should return status 204 when trying to delete a equipment using its own id', async () => {
+        const equipmentId = await _generateValidEquipmentId();
+        const response = await services.deleteEquipmentById(equipmentId);
 
+        expect(response.status).toBe(204);
+    });
+
+    it('should return status 400 when trying to delete a equipment without sends its id', async () => {
+        const response = await services.deleteEquipmentById();
+
+        expect(response.status).toBe(400);
+    });
+    
+    it('should return status 400 when trying to delete a equipment without sending fake id', async () => {
+        const FAKE_ID = -1;
+        const response = await services.deleteEquipmentById(FAKE_ID);
+
+        expect(response.status).toBe(400);
     });
 });
 
-describe('Get by id test', () => {
+describe('Search by id test', () => {
 
-    it('should give true when comparing sent id with equipment.id given', async () => {
-        const { equipment_id } = await services.createEquipment(factory.generateEquipment());
-        const response = await services.getEquipmentByField({ id: equipment_id });
+    it('should be true when comparing sent id with id returned', async () => {
+        const SENT_ID = await _generateValidEquipmentId();
+        const response = await services.getEquipmentByField({ id: SENT_ID });
+        const RETURNED_ID = response.equipment.id;
 
-        expect(response.equipment.id).toBe(equipment_id);
+        expect(SENT_ID).toBe(RETURNED_ID);
     });
+    
+    it('should return status 400 when trying to update equipment that does not exist', async () => { 
+        const response = await services.getEquipmentByField({ id: -1 });
+
+        expect(response.status).toBe(400);
+    });
+    
+    it('should return status 400 when sending nothing', async () => { 
+        const response = await services.getEquipmentByField();
+
+        expect(response.status).toBe(400);
+    });
+
 });
 
 describe('Get all test', () => {
@@ -47,5 +86,7 @@ describe('Get all test', () => {
         expect(response.status).toBe(200);
     });
 });
+
+
 
 
