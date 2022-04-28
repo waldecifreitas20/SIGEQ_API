b2b3187cebe99a7caf96e9add4580cac59e97640
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-04-28 16:11:13 -0300

    simple migrations created

--- src/database/migrations/20220427201623-equipments.js
+++ src/database/migrations/20220427201623-equipments.js
@@ -1,21 +1,49 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('equipments', {
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      company: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      model: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      heritage: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: true
+      },
+      current_location: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      warrantyExpiresAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      image: {
+        type: Sequelize.BLOB,
+        allowNull: true
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('equipments');
   }
 };
--- src/database/migrations/20220427201646-permissions.js
+++ src/database/migrations/20220427201646-permissions.js
@@ -1,21 +1,21 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('permissions', {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('permissions');
   }
 };
