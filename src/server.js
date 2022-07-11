bec884d5d844a0e7dcb6f09ab345e335249a78e6
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-07-11 19:56:16 -0300

    update server initial message

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 app.listen(process.env.PORT, async () => {
-    console.log("SERVER WAS STARTED");
+    console.log("SERVER HAS BEEN STARTED");
     console.log("LISTENING AT PORT " + process.env.PORT);
 });   
