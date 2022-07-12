45316fa3c124115d112d1e6ca281dc772abd01ab
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-07-12 20:07:05 -0300

    create permissions factory test

--- __tests__/integration/factory.test.js
+++ __tests__/integration/factory.test.js
@@ -1,4 +1,5 @@
-describe('Factory', () => {
+describe('Factory test', () => {
+    const {hasKeys} = require('../../src/utils/shorts');
 
     it('should generate a new user with all him properties', () => {
         const factory = require('../factory');
@@ -11,7 +12,7 @@ describe('Factory', () => {
         expect(newUser.cpf.length > 0).toBe(true);
     });
 
-    it('should generate a new equipment with all him properties', () => {
+    it('should generate a new equipment with all its properties', () => {
         const factory = require('../factory');
         const newEquipment = factory.generateEquipment();
 
@@ -24,5 +25,22 @@ describe('Factory', () => {
         expect(newEquipment.heritage != null).toBe(true);
         expect(newEquipment.warrantyExpireAt != null).toBe(true);
         expect(newEquipment.image != null).toBe(true);
+    });
+
+    it('should generate permissions', () => {
+        const { generatePermissions } = require('../factory');
+        const permissions = generatePermissions({
+            create: true, 
+            read: true, 
+            update: true,
+            remove: true,
+        });
+        const permissionsExpected = ['create', 'delete', 'read', 'update'];
+
+        permissions.forEach(permission => {
+            const hasPermission = permissionsExpected.indexOf(permission.name) != -1;
+            expect(hasPermission).toBe(true)
+        });
+       
     });
 });
