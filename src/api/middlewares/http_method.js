67083102e9aed5cd6b6e978d47018a8acc714793
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-08-21 12:32:19 -0300

    update response

--- src/api/middlewares/http_method.js
+++ src/api/middlewares/http_method.js
@@ -1,12 +1,6 @@
 const { getErrorResponse, ERROR_CODE } = require("../../utils/errors");
 const { ROUTES } = require("../../utils/shorts");
 
-const ERROR_RESPONSE = getErrorResponse({
-    status: 405,
-    code: ERROR_CODE.REQUEST.INVALID_HTTP_METHOD,
-    error: 'Invalid method to this request',
-});
-
 module.exports = (req, res, next) => {
     const HAS_QUERY = !!req.query;
     let url = req.url;
@@ -20,7 +14,12 @@ module.exports = (req, res, next) => {
     if (IS_EXISTING) {
         const IS_VALID_METHOD = ROUTES[url].method === req.method;
         if (!IS_VALID_METHOD) {
-            return res.status(405).send(ERROR_RESPONSE);
+            return res.status(405).send(getErrorResponse({
+                status: 405,
+                code: ERROR_CODE.REQUEST.INVALID_HTTP_METHOD,
+                error: 'Invalid HTTP method',
+                details: `Cannot reach ${url} using ${req.method} method`
+            }));
         }
     }
     
